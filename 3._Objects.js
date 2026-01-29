7fc49d6a61e3a19a9dc6b4c2d654b473452ac9a6
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-01-29 18:52:07 +0100

    objects.js excercises completed

--- 3._Objects.js
+++ 3._Objects.js
@@ -4,14 +4,18 @@
 // Exercise 1 - Retrieve value from object by key
 
 const greetings = { message: "Hello, earthling! I bring peace." };
-
+const message = greetings.message
 // Log the message 
+console.log(message)
 
 // --------------------------------------
 // Exercise 2 - Defining an object.
 
 // Create an object that has your name and age. 
-
+const person = {
+    name: "Jacob",
+    age: 31
+}
 
 // --------------------------------------
 // Exercise 3 - Add a key-value pair
@@ -19,6 +23,7 @@ const greetings = { message: "Hello, earthling! I bring peace." };
 const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
+stackOverflow.isAllowed = true;
 
 // --------------------------------------
 // Exercise 4 - Remove a property
@@ -26,7 +31,8 @@ const stackOverflow = {};
 const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
-
+delete thisSong.description;
+thisSong.about = "Just a tribute";
 
 // --------------------------------------
 
